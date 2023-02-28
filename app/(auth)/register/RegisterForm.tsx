9987f6afa73a634148f98a23601dc9f23466902e
Patrik <patrik.nuernberger@gmail.com>
2023-02-28 10:49:37 +0100

    Inplement Leaflet Map and registerPage

---
+++ app/(auth)/register/RegisterForm.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function RegisterForm() {
+  return (
+    <div>
+      <input placeholder="username" /> <input placeholder="password" />
+      <button>Submit</button>
+    </div>
+  );
+}
--- app/map.tsx
+++ app/map.tsx
@@ -1,8 +1,9 @@
 'use client';
 import './map.scss';
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 import { useEffect, useRef } from 'react';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 
 const LeafletMap = () => {
   const mapRef = useRef<L.Map>();
@@ -16,12 +17,17 @@ const LeafletMap = () => {
   return (
     <MapContainer
       center={[51.505, -0.09]}
-      zoom={13}
-      style={{ height: '400px' }}
-      whenCreated={(map) => (mapRef.current = map)}
+      zoom={23}
+      scrollWheelZoom={true}
+      className="map"
     >
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={[51.505, -0.09]} />
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>this is a popup</Popup>
+      </Marker>
     </MapContainer>
   );
 };
