4d5bb556d5971543454263d958d753df7dc29398
Patrik <patrik.nuernberger@gmail.com>
2023-04-02 20:09:00 +0200

    Increase Testing capability

--- playwright/tests/navigate.spec.ts
+++ playwright/tests/navigate.spec.ts
@@ -6,8 +6,10 @@ test('navigation test', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Winzer' })).toBeVisible();
 
   await expect(page.locator('h1')).toHaveText('Winzer');
-
+  await page.goto('http://localhost:3000/login');
   await expect(page).toHaveURL('http://localhost:3000/login');
+  await page.goto('http://localhost:3000/register');
   await expect(page).toHaveURL('http://localhost:3000/register');
+  await page.goto('http://localhost:3000/host');
   await expect(page).toHaveURL('http://localhost:3000/host');
 });
